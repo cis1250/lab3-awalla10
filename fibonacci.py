93aba2a292dfcc6cf8ba4d30587f6670be9f5b32
arianna wallace <awalla10@uoguelph.ca>
2025-10-03 08:45:59 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -5,3 +5,9 @@
 # Prompt the user for the number of terms.
 # Validate that the input is a positive integer.
 # Use a for loop to print the Fibonacci sequence up to that many terms.
+
+user_input = input("Please enter a value:\n")
+if (user_input > 0):
+  print("Calculating Fibonacci Sequence")
+else:
+  print("Invaild, Try again")
