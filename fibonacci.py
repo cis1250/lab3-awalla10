cd6b4e7ca411775cce5cdb70ab00fcd1eee17f7a
arianna wallace <awalla10@uoguelph.ca>
2025-10-03 13:10:17 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -6,15 +6,25 @@
 # Validate that the input is a positive integer.
 # Use a for loop to print the Fibonacci sequence up to that many terms.
 
-user_input = input("Please enter a value:\n")
-user_input_int = int(user_input) 
+
 while True:
   try:
-      if (user_input_int > 0):
+    user_input = input("Please enter a value:\n")
+    user_input_int = int(user_input) 
+    if (user_input_int > 0):
         print("Calculating Fibonacci Sequence")
-      else:
+        a, b = 0, 1
+        print(a)  # Print the first term (0)
+        print(b)  # Print the second term (1)
+    
+        for _ in range(2, user_input_int):  # Start from the 3rd term
+            next_term = a + b
+            print(next_term)
+            a = b
+            b = next_term
+        break
+    else:
         print("Invalid number, try agian")
 
   except ValueError:
         print("Invalid input.")
-  
