ae5b9739aa64f3ddf45adaf9236d9359f0e7160f
arianna wallace <awalla10@uoguelph.ca>
2025-10-03 08:59:57 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -7,7 +7,14 @@
 # Use a for loop to print the Fibonacci sequence up to that many terms.
 
 user_input = input("Please enter a value:\n")
-if (user_input > 0):
-  print("Calculating Fibonacci Sequence")
-else:
-  print("Invaild, Try again")
+user_input_int = int(user_input) 
+while True:
+  try:
+      if (user_input_int > 0):
+        print("Calculating Fibonacci Sequence")
+      else:
+        print("Invalid number, try agian")
+
+  except ValueError:
+        print("Invalid input.")
+  
